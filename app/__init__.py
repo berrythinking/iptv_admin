fc5e379e3b004620532685b48eeaadeeab88985c
topilski <atopilski@gmail.com>
2019-01-19 08:10:32 -0500

    Init func

--- app/__init__.py
+++ app/__init__.py
@@ -5,29 +5,36 @@ from flask_mail import Mail
 from flask_bootstrap import Bootstrap
 from flask_babel import Babel
 from flask_socketio import SocketIO
-
-app = Flask(__name__)
-
-# load configs
-app.config.from_pyfile('config/public_config.py', silent=False)
-app.config.from_pyfile('config/config.py', silent=True)
-
-# modules
-bootstrap = Bootstrap(app)
-babel = Babel(app)
-db = MongoEngine(app)
-mail = Mail(app)
-socketio = SocketIO(app)
-
-login_manager = LoginManager(app)
-
 from app.home.view import HomeView
-HomeView.register(app)
-
 from app.user.view import UserView
-UserView.register(app)
 
-login_manager.login_view = "HomeView:login"
+
+def init_project(static_folder, *args):
+    app = Flask(__name__, static_folder=static_folder)
+
+    for file in args:
+        app.config.from_pyfile(file, silent=False)
+
+    bootstrap = Bootstrap(app)
+    babel = Babel(app)
+    db = MongoEngine(app)
+    mail = Mail(app)
+    socketio = SocketIO(app)
+    login_manager = LoginManager(app)
+
+    HomeView.register(app)
+    UserView.register(app)
+
+    login_manager.login_view = "HomeView:login"
+
+    return app, bootstrap, babel, db, mail, socketio, login_manager
+
+
+app, bootstrap, babel, db, mail, socketio, login_manager = init_project(
+    'static',
+    'config/public_config.py',
+    'config/config.py',
+)
 
 
 def page_not_found(e):
